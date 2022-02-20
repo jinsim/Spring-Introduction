223cceee13433bb381475740675dadb5de8ba844
jinsim <simsime@korea.ac.kr>
2022-02-20 13:16:11 +0900

    현재까지 작업한 내용 커밋

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,52 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    // 웹 애플리케이션에서, /hello로 들어오면, 아래의 메소드를 호출해줌.
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    // 외부에서 파라미터를 받을 것임.
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    // HTTP의 Body에 return에 있는 데이터를 직접 넣어주겠다는 의미.
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    // static class 로 만들면, 상위 클래스를 안에서 사용할 수 있음.
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
